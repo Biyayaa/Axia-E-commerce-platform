18abb2485548f903ee928e9e2ab984fc7d989fa3
Biyayaa <onaleye.ap@gmail.com>
2025-08-05 17:20:05 +0100

    authController setup for register and login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get('/', (req, res)=>{
     res.send('E-commerce platform API')
 });
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre('save', async function () {
+  try {
+    if (this.isModified('password')) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  } catch (err) {
+    throw new Error(`Password hashing failed: ${err.message}`);
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
